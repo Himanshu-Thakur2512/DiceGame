37da96bc441e0557b9db56f990016a5ef57581ca
Himanshu-Thakur2512 <67829852+Himanshu-Thakur2512@users.noreply.github.com>
2020-08-31 12:43:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var a= document.querySelector("img");
+a.setAttribute("src", randomImageSource);
+
+var randomImageNumber2= Math.floor( Math.random() * 6) + 1;
+var randomDiceImage2= "dice"+ randomImageNumber2+ ".png";
+var randomImageSource2= "images/"+randomDiceImage2;
+var b= document.querySelectorAll("img")[1];
+b.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomImageNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 1 wins!!";
+}
+else if(randomNumber1 < randomImageNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 wins!!";
+}
+
+else{
+    document.querySelector("h1").innerHTML="Try Again!";
+}
